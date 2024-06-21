368eb0398be676eef95a4b7321ef766ec50ec9c1
iysrustandi <iyusrustandi2@gmail.com>
2024-06-21 19:43:52 +0700

    update style with tailwind

---
+++ script.js
@@ -0,0 +1,50 @@
+//responsive navbar
+var toggleOpen = document.getElementById('toggleOpen');
+var toggleClose = document.getElementById('toggleClose');
+var collapseMenu = document.getElementById('collapseMenu');
+
+function handleClick() {
+  if (collapseMenu.style.display === 'block') {
+    collapseMenu.style.display = 'none';
+  } else {
+    collapseMenu.style.display = 'block';
+  }
+}
+
+toggleOpen.addEventListener('click', handleClick);
+toggleClose.addEventListener('click', handleClick);
+
+//about me active tab
+function opentab(tabName) {
+  var tabContents = document.getElementsByClassName('tab-contents');
+
+  for (var i = 0; i < tabContents.length; i++) {
+    tabContents[i].classList.add('hidden');
+    tabContents[i].classList.remove('block');
+  }
+
+  document.getElementById(tabName).classList.remove('hidden');
+  document.getElementById(tabName).classList.add('block');
+
+  var tabLinks = document.getElementsByClassName('tab-links');
+  for (var i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].classList.remove('active-link');
+  }
+  event.currentTarget.classList.add('active-link');
+}
+
+//contact form google https://beproblemsolver.com/html-form-to-google-sheet/#google_vignette
+window.addEventListener('load', function () {
+  const form = document.getElementById('my-form');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    }).then(() => {
+      alert('Message sent succesfully');
+    });
+  });
+});
